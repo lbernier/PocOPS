d88f700b1b9957ef336f383dbd53006f71d35b64
lbernier <leandro_bernier@yahoo.com>
2022-02-18 18:48:09 -0500

    modificacion para heroku

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-/*
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/poc-ops'));
-app.get('/', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/poc-ops/index.html'));});
-app.listen(process.env.PORT || 8080);
-*/
 
 const express = require("express");
 
