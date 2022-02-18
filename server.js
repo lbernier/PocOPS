f42c55ba8f7c0c3ed3b6be6d8c18d38396c6fdf8
lbernier <leandro_bernier@yahoo.com>
2022-02-18 18:18:28 -0500

    modificacion para heroku

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+/*
 const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/poc-ops'));
-app.get('/*', function(req,res) {
+app.get('/', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/poc-ops/index.html'));});
 app.listen(process.env.PORT || 8080);
+*/
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("./dist/poc-ops"));
+
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "dist/poc-ops" });
+});
+
+app.listen(process.env.PORT || 8080);
+
 console.log(`Running on port ${process.env.PORT || 8080}`);
